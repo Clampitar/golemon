7e6691060a57ff11e6b129ad3f92d21a83b8e5a1
John Muller <johnmuller575@gmail.com>
2020-05-30 17:50:40 -0400

    walk cycle

--- core/src/com/mygdx/game/Character.java
+++ core/src/com/mygdx/game/Character.java
@@ -10,35 +10,68 @@ public class Character {
 
     protected final int X_SCALE = 32;
     protected final int Y_SCALE = 32;
-    protected final int WALK_CYCLE_LENGTH = 60;
 
-    protected int x = 0;
-    protected int y = 0;
-    protected int walkCycle = 0;
+    protected float x = 0;
+    protected float y = 0;
+    protected float walkCycle = 0;
+
+    protected int direction = Input.DOWN;
 
 
-
-    public Character(Texture texture){
+    public Character(Texture texture) {
         this.texture = texture;
-        region = new TextureRegion(texture, 0, 0, X_SCALE ,Y_SCALE);
+        region = new TextureRegion(texture, 0, direction * Y_SCALE, X_SCALE, Y_SCALE);
     }
 
     public TextureRegion getRegion() {
         return region;
     }
 
-    public void walk(int xInput, int yInput){
-        x += xInput;
-        y += yInput;
-        walkCycle++;
-        walkCycle %= WALK_CYCLE_LENGTH;
+    public void walk(int xInput, int yInput) {
+        if (xInput != 0 || yInput != 0)
+            if (xInput != 0 && yInput != 0) {
+                //diagonals
+                x += xInput * Math.sin(Math.PI / 4);
+                y += yInput * Math.sin(Math.PI / 4);
+            } else {
+                x += xInput;
+                y += yInput;
+                if (xInput > 0) {
+                    direction = Input.RIGHT;
+                }
+                if (xInput < 0) {
+                    direction = Input.LEFT;
+                }
+                if (yInput > 0) {
+                    direction = Input.UP;
+                }
+                if (yInput < 0) {
+                    direction = Input.DOWN;
+                }
+            }
+        walkCycle += MyGdxGame.STEP;
+        walkCycle %= 1;
+        updateSprite();
     }
 
-    public int getX() {
+
+    private void updateSprite() {
+        int xSprite = 0;
+        int ySprite = 0;
+        ySprite = Y_SCALE * direction;
+        if (walkCycle > 0.25 && walkCycle < 0.5) {
+            xSprite = X_SCALE * 1;
+        } else if (walkCycle > 0.75) {
+            xSprite = X_SCALE * 2;
+        }
+        region.setRegion(xSprite, ySprite, X_SCALE, Y_SCALE);
+    }
+
+    public float getX() {
         return x;
     }
 
-    public int getY() {
+    public float getY() {
         return y;
     }
 }
